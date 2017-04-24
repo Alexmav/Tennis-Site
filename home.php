e5aadab47cc7a55d785209f64734d2aa7b502643
Alex Maveyraud <alex.maveyraud@gmail.com>
2017-04-24 09:58:01 +0200

    test

--- home.php
+++ home.php
@@ -20,12 +20,35 @@
 <div class="w3-row-padding w3-padding-64 w3-container">
     <div class="w3-content">
         <div class="w3-twothird">
-            <h1>Lorem Ipsum</h1>
-            <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+            <h1>Matchs en coours</h1>
 
-            <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat.test</p>
+                <thead> <!-- En-tête du tableau -->
+                <tr>
+                    <th>Adversaire 1</th>
+                    <th>Score</th>
+                    <th>Adversaire 2</th>
+                    <th>Tournoi</th>
+                </tr>
+                </thead>
+
+
+
+                <tbody> <!-- Corps du tableau -->
+                <tr>
+                    <th>NAdal</th>
+                    <th>6-4</th>
+                    <th>Djocko</th>
+                    <th>Tournoi</th>
+                </tr>
+                <tr>
+                    <th>Monfils</th>
+                    <th>6-3</th>
+                    <th>Federer</th>
+                    <th>Tournoi</th>
+                </tr>
+
+                </tbody>
+            </table>
         </div>
 
         <div class="w3-third w3-center">
