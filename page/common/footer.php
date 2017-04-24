8782f600fa4b6a8536389ee05d96cd1a99451217
Datare <bdallard.dallardbastien@gmail.com>
2017-04-24 09:41:42 +0100

    BDA

---
+++ page/common/footer.php
@@ -0,0 +1,6 @@
+<!-- Footer -->
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-16">
+  <footer class="w3-container w3-center w3-opacity">
+    <p  class="w3-margin w3-xlarge">Projet AcE</p>
+  </footer>
+</div>
---
+++ page/common/header.php
@@ -0,0 +1,8 @@
+<?php
+ include "./page/common/navbar.html";
+?>
+
+<!-- Header -->
+<header class="w3-container w3-red w3-center" style="padding:40px 16px">
+    <h1 class="w3-margin w3-jumbo">AcE</h1>
+</header>
